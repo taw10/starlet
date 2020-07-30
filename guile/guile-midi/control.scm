095485a24a01cf01fb8dfb76d103c17d8b1ae9a8
Thomas White <taw@physics.org>
2020-07-30 23:09:02 +0200

    Simplify MIDI control

--- guile/guile-midi/control.scm
+++ guile/guile-midi/control.scm
@@ -1,101 +1,87 @@
 (define-module (guile-midi control)
+  #:use-module (oop goops)
+  #:use-module (ice-9 atomic)
   #:use-module (ice-9 threads)
   #:use-module (ice-9 binary-ports)
-  #:export (make-midi-port
-            midi-cc-value
-            send-midi-note))
+  #:use-module (srfi srfi-1)
+  #:export (start-midi-control
+            make-midi-controller
+            get-controller-value))
 
 
-(define (make-midi-port device-name listen-channel)
-
-  (let ((cc-vals (make-array 0 128))
-        (midi-port (open-file device-name "r+0b")))
+(define cc-list (make-atomic-box '()))
 
 
-    ;; Send a note off command
-    (define (send-noteoff note)
-      (put-u8 midi-port
-              (+ #b10000000 listen-channel))
-      (put-u8 midi-port note)
-      (put-u8 midi-port 0))
+(define-class <midi-control> (<object>)
+
+  (channel
+   #:init-keyword #:channel
+   #:getter get-channel)
+
+  (cc-number
+   #:init-keyword #:cc-number
+   #:getter get-cc-number)
+
+  (value-box
+   #:init-form (make-atomic-box 0)
+   #:getter get-value-box))
 
 
-    ;; Send a note on command
-    (define (send-note note velocity)
-      (put-u8 midi-port
-              (+ #b10010000 listen-channel))
-      (put-u8 midi-port note)
-      (put-u8 midi-port velocity))
+(define (get-controller-value a)
+  (atomic-box-ref (get-value-box a)))
 
 
-    ;; Get a CC value
-    (define (get-cc-value controller-number)
-      (array-ref cc-vals
-                 controller-number))
+(define* (make-midi-controller
+          #:key (channel 1) (cc-number 1))
+  (let ((new-controller (make <midi-control>
+                          #:channel channel
+                          #:cc-number cc-number)))
+    (atomic-box-set! cc-list
+                     (cons new-controller
+                           (atomic-box-ref cc-list)))
+    new-controller))
 
 
-    (define (run-midi)
-
-        (let again ()
-          (letrec* ((status-byte (get-u8 midi-port))
-                    (channel (bit-extract status-byte 0 4))
-                    (command (bit-extract status-byte 4 8)))
-
-            (when (eq? channel listen-channel)
-              (case command
-
-                ;; Note on
-                ((9) (let ((note (get-u8 midi-port))
-                           (vel (get-u8 midi-port)))
-                       (display "Note = ")
-                       (display (number->string note 16))
-                       (display " velocity = ")
-                       (display vel)
-                       (newline)))
-
-                ;; Control value
-                ((11) (let* ((controller-number (get-u8 midi-port))
-                             (value (get-u8 midi-port)))
-                        (array-set! cc-vals
-                                    value
-                                    controller-number)))))
+(define (handle-cc-change channel cc-number value)
+  (for-each (lambda (a)
+              (atomic-box-set! (get-value-box a) value))
+            (filter (lambda (a)
+                      (and (eq? cc-number (get-cc-number a))
+                           (eq? channel (get-channel a))))
+                    (atomic-box-ref cc-list))))
 
 
-            (again))))
-
-    ;; Clear out any LEDs by first sending note-on with velocity zero
-    (for-each (lambda (n)
-                (send-note n 0))
-              (iota 128 0))
-
-    ;; ... and subsequently sending note-off
-    (for-each (lambda (n)
-                (send-noteoff n))
-              (iota 128 0))
-
-    (make-thread run-midi)
-
-    (lambda args
-      (apply
-       (case (car args)
-         ((get-cc-value) get-cc-value)
-         ((send-note) send-note))
-       (cdr args)))))
+(define (scale-127-100 n)
+  (/ (* n 100) 127))
 
 
-(define-syntax midi-cc-value
-  (lambda (x)
-    (syntax-case x ()
-      ((_ port controller-number)
-       #'(port 'get-cc-value controller-number)))))
+(define (start-midi-control device-name)
+  (let ((midi-port (open-file device-name "r+0b")))
 
+    (begin-thread
+     (let again ()
 
-(define-syntax send-midi-note
-  (lambda (x)
-    (syntax-case x ()
+       (letrec* ((status-byte (get-u8 midi-port))
+                 (channel (bit-extract status-byte 0 4))
+                 (command (bit-extract status-byte 4 8)))
 
-      ((_ port note velocity)
-       #'(port 'send-note note velocity))
+         (case command
 
-      ((_ port note)
-       #'(port 'send-note note 127)))))
+           ;; Note on
+           ((9) (let ((note (get-u8 midi-port))
+                      (vel (get-u8 midi-port)))
+                  (display "Note = ")
+                  (display (number->string note 16))
+                  (display " velocity = ")
+                  (display vel)
+                  (newline)))
+
+           ;; Control value
+           ((11) (let* ((cc-number (get-u8 midi-port))
+                        (value (get-u8 midi-port)))
+                   (handle-cc-change channel
+                                     cc-number
+                                     (scale-127-100 value)))))
+
+         (yield)
+         (again))))))
