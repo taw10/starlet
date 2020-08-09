6f97fd3ffd1579e04efee17500877dcc64ef5f1c
Thomas White <taw@physics.org>
2020-08-09 08:28:42 +0200

    Formatting

--- guile/starlet/fixture-library/generic.scm
+++ guile/starlet/fixture-library/generic.scm
@@ -9,8 +9,11 @@
    #:init-form
    (list
 
-    (make <fixture-attribute> #:name 'intensity
-          #:range '(0 100) #:type 'continuous #:home-value 0
-          #:translator (lambda (universe start-addr value set-dmx)
-                         (set-dmx universe start-addr
-                                  (percent->dmxval value)))))))
+    (make <fixture-attribute>
+      #:name 'intensity
+      #:range '(0 100)
+      #:type 'continuous
+      #:home-value 0
+      #:translator (lambda (universe start-addr value set-dmx)
+                     (set-dmx universe start-addr
+                              (percent->dmxval value)))))))
