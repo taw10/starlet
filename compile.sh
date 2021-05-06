69b974597f9153689373ddb0e316dd406e3718ac
Thomas White <taw@physics.org>
2021-05-06 21:29:50 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-set -x
 g++ guile-ola.cpp -o libguile-ola.so -lola --shared -fPIC \
                   `pkg-config --cflags --libs guile-3.0`
 
 echo Now run: sudo cp libguile-ola.so /usr/local/lib64/
+echo Then: sudo ldconfig
