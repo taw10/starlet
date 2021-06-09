602e45a6de90ec9ab4caea2f2bdd2e3ee60edf22
Thomas White <taw@physics.org>
2021-06-09 18:12:59 +0200

    Compile libguile-ola using Meson

--- compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-g++ guile-ola.cpp -o libguile-ola.so -lola --shared -fPIC \
-                  `pkg-config --cflags --libs guile-3.0`
-
-echo Now run: sudo cp libguile-ola.so /usr/local/lib64/
-echo Then: sudo ldconfig
