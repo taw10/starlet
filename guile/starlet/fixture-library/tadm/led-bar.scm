1392cdcbdd18f2bc7c137edcccc2c833c86b4e04
Thomas White <taw@physics.org>
2021-10-23 11:45:54 +0200

    Add TadM LED bar

---
+++ guile/starlet/fixture-library/tadm/led-bar.scm
@@ -0,0 +1,44 @@
+;;
+;; starlet/fixture-library/tadm/led-bar.scm
+;;
+;; Copyright © 2020-2021 Thomas White <taw@bitwiz.org.uk>
+;;
+;; This file is part of Starlet.
+;;
+;; Starlet is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;
+(define-module (starlet fixture-library tadm led-bar)
+  #:use-module (oop goops)
+  #:use-module (starlet fixture)
+  #:use-module (starlet colours)
+  #:export (<tadm-led-bar>))
+
+
+(define-class <tadm-led-bar> (<fixture>)
+  (attributes
+   #:init-form (list
+                (attr-continuous 'intensity '(0 100) 0)
+                (attr-colour 'colour white))))
+
+
+(define-method (scanout-fixture (fixture <tadm-led-bar>)
+                                get-attr set-chan8 set-chan16)
+
+  (let ((intensity (get-attr 'intensity))
+        (rgb (colour-as-rgb (get-attr 'colour))))
+    (set-chan8 1 (percent->dmxval8 intensity))
+    (set-chan8 2 (percent->dmxval8 (car rgb)))
+    (set-chan8 3 (percent->dmxval8 (cadr rgb)))
+    (set-chan8 4 (percent->dmxval8 (caddr rgb)))))
+
