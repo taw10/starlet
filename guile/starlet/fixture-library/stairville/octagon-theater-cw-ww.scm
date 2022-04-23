8f4186b42dbffbaf461a2b9c3cbcaf6b4c4cadd9
Thomas White <taw@physics.org>
2022-04-23 16:08:00 +0200

    Fixture library: Add Stairville Octagon Theater CW/WW

---
+++ guile/starlet/fixture-library/stairville/octagon-theater-cw-ww.scm
@@ -0,0 +1,45 @@
+;;
+;; starlet/fixture-library/stairville/octagon-theater-cw-ww.scm
+;;
+;; Copyright © 2020-2022 Thomas White <taw@bitwiz.me.uk>
+;;
+;; This file is part of Starlet.
+;;
+;; Starlet is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;
+(define-module (starlet fixture-library stairville octagon-theater-cw-ww)
+  #:use-module (oop goops)
+  #:use-module (starlet fixture)
+  #:use-module (starlet colours)
+  #:export (<stairville-octagon-theater-cw-ww>))
+
+
+(define-class <stairville-octagon-theater-cw-ww> (<fixture>)
+  (attributes
+   #:init-form (list
+                (attr-continuous 'intensity '(0 100) 0)
+                (attr-continuous 'colour-temperature '(2800 6400) 4600))))
+
+
+(define-method (scanout-fixture (fixture <stairville-octagon-theater-cw-ww>)
+                                get-attr set-chan8 set-chan16)
+
+  (let ((intensity (get-attr 'intensity))
+        (coltemp (get-attr 'colour-temperature)))
+    (set-chan8 1 (scale-and-clamp-to-range coltemp '(2800 6400) '(0 255)))
+    (set-chan8 2 (scale-and-clamp-to-range coltemp '(2800 6400) '(255 0)))
+    (set-chan8 3 0)  ;; Strobe
+    (set-chan8 4 0)  ;; Mode (0-15 = direct control)
+    (set-chan8 5 (percent->dmxval8 intensity))))
+
